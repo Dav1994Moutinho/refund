0cad530508c23f799c8782e5fce47cd03929cf93
David Moutinho <davidmoutinho1994@gmail.com>
2024-12-06 11:31:01 -0300

    Selecionando outros elementos do formulario, criando a função de formatar a moeda para BRL.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
 // Seleciona os elementos do formulário.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
+// Captura o evento de input para formatar o valeor.
 amount.oninput = () => {
+    // Otem o valor do input e remove os caracteres nao numéricos.
     let value = amount.value.replace(/\D/g, "")
-    amount.value = value
+
+    // Transformar o valor em centavos
+    value = Number(value) / 100
+    // Atualiza o valor do input.
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no pradrão BRL (Real rasileiro).
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+    // Retorna o valor formatado.
+    return value
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault()
 }
