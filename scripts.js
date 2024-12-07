cfcd714ede2b6c8cd80694531cf67207f7d64ab0
David Moutinho <davidmoutinho1994@gmail.com>
2024-12-06 23:18:19 -0300

    Adicionando nome e categoria na div das informações das despesas.

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+// Seleciona os elementos da lista.
+const expenseList = document.querySelector("ul")
+
 // Captura o evento de input para formatar o valeor.
 amount.oninput = () => {
     // Otem o valor do input e remove os caracteres nao numéricos.
@@ -38,5 +41,43 @@ form.onsubmit = (event) => {
         amount: amount.value,
         created_at: new Date(),
     }
-    console.log(newExpense)
+    // Chama a função que irá adicionar o item na lista.
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        // Cria o elemento para adicionar o item (li) na lista (ul).
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+        // Cria o icone da categoria.
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+
+        // Cria a info da despesa.
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
+
+        // Cria o nome da despesa.
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        // Cria categoria da despesa.
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+
+        // Adiciona nome e categoria na div das informações das despesas.
+        expenseInfo.append(expenseName, expenseCategory)
+
+        // Adiciona as informações no item.
+        expenseItem.append(expenseIcon, expenseInfo)
+        
+        // Adiciona o item na lista
+        expenseList.append(expenseItem)
+    } catch (error) {
+        alert("Não foi possivel atualizar a lista de despesas.")
+        console.log(error)
+    }
 }
